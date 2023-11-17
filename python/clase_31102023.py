1b0ab4eb788941ceb97d2b99bd0134d5166e70c5
dvidCR <dvidcr100@gmail.com>
2023-11-17 16:00:21 +0100

    Decimo Commit

--- python/clase_31102023.py
+++ python/clase_31102023.py
@@ -5,12 +5,24 @@ class Contact:
         self.name = name
         self.email = email
         Contact.all_contacts.append(self)
+    
+    def imprimirContactoactual(self):
+        print(f'{self.name + " --> " + self.email}')
         
 class Supplier(Contact):
     def order(self, order):
         print("Si este fuera un sistema real, enviaríamos el pedido '{}' a '{}'".format(order, self.name))
 
+c = Contact("Jose", "jose@gmail.net")
+
 name = input("Escribe tu nombre: ")
 email = input("Escribe tu email: ")
-cont = Contact(name,email)
-supp  = Supplier.order(cont,"Cascos")
+
+supp = Supplier(name, email)
+
+supp.order("Esta es la orden 1")
+
+supp.imprimirContactoactual()
+
+for i in Contact.all_contacts:
+    print(i.name, i.email)
--- python/tarea_07112023/Ejercicio1/persona.py
+++ python/tarea_07112023/Ejercicio1/persona.py
@@ -1,4 +1,5 @@
 class Persona:
+    
     def __init__(self, nombre, edad, DNI):
         self.nombre = nombre
         self.edad = edad
